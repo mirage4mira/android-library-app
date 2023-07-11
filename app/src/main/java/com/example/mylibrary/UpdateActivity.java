77aaece95da1abe54940c2ac9fca1509514ac1e7
unknown <mirage4mira@gmail.com>
2023-07-11 19:30:13 +0800

    first commit

---
+++ app/src/main/java/com/example/mylibrary/UpdateActivity.java
@@ -0,0 +1,102 @@
+package com.example.mylibrary;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    EditText title_input, author_input, pages_input;
+    String id,title,author,pages;
+    Button update_button,delete_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+
+        title_input = findViewById(R.id.title_input2);
+        author_input = findViewById(R.id.author_input2);
+        pages_input = findViewById(R.id.pages_input2);
+        update_button = findViewById(R.id.update_button);
+        delete_button = findViewById(R.id.delete_button);
+
+        getAndSetIntentData();
+
+        ActionBar ab = getSupportActionBar();
+
+        if(ab != null){
+            ab.setTitle(title);
+        }
+        update_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Toast.makeText(UpdateActivity.this,title_input.getText(),Toast.LENGTH_SHORT).show();
+
+                MyDatabaseHelper myDB = new MyDatabaseHelper(UpdateActivity.this);
+
+                title = title_input.getText().toString();
+                author = author_input.getText().toString();
+                pages = pages_input.getText().toString();
+                myDB.updateData(id,title,author,pages);
+            }
+        });
+
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmDialog();
+            }
+        });
+
+    }
+
+    void getAndSetIntentData(){
+        if(getIntent().hasExtra("id") && getIntent().hasExtra("title") && getIntent().hasExtra("author") && getIntent().hasExtra("pages")){
+            id = getIntent().getStringExtra("id");
+            title = getIntent().getStringExtra("title");
+            author = getIntent().getStringExtra("author");
+            pages = getIntent().getStringExtra("pages");
+
+            title_input.setText(title);
+            author_input.setText(author);
+            pages_input.setText(pages);
+
+        }else{
+            Toast.makeText(this,"No data.",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void confirmDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete " + title + " ?");
+        builder.setMessage("Are you sure you want to delete " + title + " ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(UpdateActivity.this);
+                myDB.deleteOneRow(id);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        builder.create().show();
+    }
+
+}
